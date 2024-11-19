5fa20791ba68f1d394d5680dd28fd7f2c4d24c76
lluuww0112 <tgur0620@gmail.com>
2024-11-19 20:17:40 +0900

    updated

--- SH/API/fridge.py
+++ SH/API/fridge.py
@@ -60,7 +60,6 @@ def delete_Refri():
 # 냉장고에 음식 추가 또는 업데이트
 # 동일한 음식이 다른 날짜로 추가되는 경우 기존 데이터를 수정하는 것이 아닌 새 행을 추가
 # 최종적으로 현재 냉장고 재고 취합 데이터를 유저에게 보여주는 방식
-    # 사과(15일, 2), 사과(17일, 1)라면 취합 한 형태로 사과(3)으로 보여주도록 함
 @fridge_BP.route('/add_food', methods=['POST']) 
 def add_food():
     # 요청 데이터 가져오기
@@ -72,7 +71,6 @@ def add_food():
     cat_name = food_data.get('Cat_Name')
     count = food_data.get('Count')
     type_ = food_data.get('Type')
-
 
     # 필수 필드 확인
     if not all([refr_id, food_name, input_date, count, cat_name, exp_date, type_]):
@@ -106,14 +104,24 @@ def add_food():
         if result:
             # 이미 냉장고에 동일 정보의 음식이 있을 경우 수량 및 유통기한 업데이트
             new_count = int(result['count']) + int(count)
-            update_sql = '''
-                UPDATE contain 
-                SET count = %s, input_date = %s, exp_date = %s, type = %s
-                WHERE food_name = %s AND cat_name = %s AND Refri_ID = %s AND
-                    input_date = %s AND exp_date = %s 
-            '''
-            cursor.execute(update_sql, (new_count, input_date_str, exp_date_str, type_, food_name, cat_name, refr_id, input_date_str, exp_date_str))
-            message = "재고가 업데이트 되었습니다."
+
+            if new_count == 0: # new_count가 0이면 해당 행을 삭제
+                delete_sql = '''
+                    DELETE FROM contain 
+                    WHERE food_name = %s AND cat_name = %s AND Refri_ID = %s AND
+                        input_date = %s AND exp_date = %s
+                '''
+                cursor.execute(delete_sql, (food_name, cat_name, refr_id, input_date_str, exp_date_str))
+                message = "해당 재고가 삭제되었습니다."
+            else: # new_count가 0이 아니면 해당 행을 업데이트
+                update_sql = '''
+                    UPDATE contain 
+                    SET count = %s, input_date = %s, exp_date = %s, type = %s
+                    WHERE food_name = %s AND cat_name = %s AND Refri_ID = %s AND
+                        input_date = %s AND exp_date = %s 
+                '''
+                cursor.execute(update_sql, (new_count, input_date_str, exp_date_str, type_, food_name, cat_name, refr_id, input_date_str, exp_date_str))
+                message = "재고가 업데이트 되었습니다."
         else:
             # 냉장고에 음식이 없을 경우 새로 추가
             insert_sql = '''
@@ -145,7 +153,6 @@ def delete_food():
     count = food_data.get('Count')
     type_ = food_data.get('Type')
     
-
     # 필수 필드 확인
     if not all([refr_id, food_name, input_date, exp_date]):
         return jsonify({"error": "Refri_ID, food_name, input_date, and exp_date are required"}), 400
